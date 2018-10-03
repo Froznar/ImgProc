2ce8bae970909759655b2058e56687634e5d2597
Francisco Marin <paracelso.silfos@hotmail.com>
2018-10-03 17:25:46 -0500

    Solucionando fourier

--- ImgProc/fourier.py
+++ ImgProc/fourier.py
@@ -4,12 +4,13 @@ import math
 import cmath
 from matplotlib import pyplot as plt
 
-im = Image.open('bw.jpg').convert('L', (0.2989, 0.5870, 0.1140, 0))  # To grayscale.
+im = Image.open('bw.png').convert('L')  # To grayscale.
 pix = im.load()
 x_size = im.size[0]
 y_size = im.size[1]
 fur_result = pix
 invfur_result = pix
+Cfur_result = [[0.0 for x in range(x_size)] for y in range(y_size)]
 conv_result = output = np.zeros_like(pix)
 #Kernel = [[0 for x in range(w)] for y in range(h)] 
 Kernel = np.array([[1,1,1],
@@ -21,62 +22,59 @@ def most_common(lst):
     return max(set(lst), key=lst.count)
 
 
-def Furier():
+def Fourier():
     print("Realizando Furier")
-    sumatoria = 0
-     
     for u in range(0, x_size): #N
-        sum1 = 0
-        sum1i = 0
         for v in range(0, y_size): #M
-            sum2 = 0
-            sum2i = 0
+            sum2 = complex(0)
+            #sum2i = 0
             for x in range(0, x_size): #N
                 sum3 = 0
-                sum3i = 0
+                #sum3i = 0
                 for y in range(0, y_size): #M                        
-                    e = cmath.exp(- 1j * math.pi * 2.0 * ((u*x / x_size) + (v*y / y_size)))
-                    sum3 += e * pix[x,y]
+                    e = cmath.exp(-1j * math.pi * 2.0 * ((float(u*x) / x_size) + (float(v*y) / y_size)))
+                    #e = pow(-1j, (((u*x) / x_size) + ((v*y) / y_size))) 
+                    sum2 += e * complex(pix[x,y])
                     #ang = 2*math.pi*(u*x/x_size + v*y/y_size)
                     #sum3 = sum3 + (pix[x,y] * (math.cos(ang)))
                     #sum3i = sum3i + (pix[x,y] * (- math.sin(ang)))
-                sum2 += sum3
+                
                 #sum2i = sum2 + sum3i
+            
+            Cfur_result[u][v] = (sum2/(x_size*y_size))
             fur_result[u,v] = (int)(sum2.real/(x_size*y_size))
+    
+    #for i in range(0, x_size): #N
+    #    for j in range(0, y_size): #M
+    #        pix[i,j] = fur_result[i,j]
+    #im.save('FurResult.png')  # Save
 
-    for i in range(0, x_size): #N
-        for j in range(0, y_size): #M
-            pix[i,j] = fur_result[i,j]
-    im.save('FurResult.jpg')  # Save
 
 
-
-def InvFurier():
+def InvFourier():
     print("Realizando INV Furier")
-    sumatoria = 0    
     for u in range(0, x_size): #N
-        sum1 = 0
-        sum1i = 0
         for v in range(0, y_size): #M
-            sum2 = 0
+            sum2 = complex(0)
             sum2i = 0
             for x in range(0, x_size): #N
                 sum3 = 0
                 sum3i = 0
                 for y in range(0, y_size): #M
-                    e = cmath.exp(1j * math.pi * 2.0 * ((u*x / x_size) + (v*y / y_size)))
-                    sum3 += e * invfur_result[x,y]
+                    e = cmath.exp(1j * math.pi * 2.0 * ((float(u*x) / x_size) + (float(v*y) / y_size)))
+                    #e = pow(1j, (((u*x) / x_size) + ((v*y) / y_size))) 
+                    sum2 += e * Cfur_result[x][y]
                     #ang = 2*math.pi*(u*x/x_size + v*y/y_size)
                     #sum3 = sum3 + (fur_result[x,y] * (math.cos(ang)))
                     #sum3i = sum3i + (fur_result[x,y] * (math.sin(ang)))
-                sum2 = sum2 + sum3
+                #sum2 = sum2 + sum3
                 #sum2i = sum2 + sum3i
-            invfur_result[u,v] = (int)(sum2.real/(x_size*y_size))
+            invfur_result[u,v] = (int)(sum2.real)
 
     for i in range(0, x_size): #N
         for j in range(0, y_size): #M
             pix[i,j] = invfur_result[i,j]
-    im.save('InvFurResult.jpg')  # Save
+    im.save('InvFurResult.png')  # Save
 
 
 def convolution():
@@ -116,9 +114,9 @@ def convolution():
 
 def main():
     print("imagen de tamaño:", x_size, y_size)   
-    #Furier()
-    #InvFurier()
-    convolution()
+    Fourier()
+    InvFourier()
+    #convolution()
 
 if __name__ == '__main__':
     main()
